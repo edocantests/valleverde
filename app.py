c67d64c12fb0d089eecb36ece8c039e84e467064
edocantests <rwc.cupc@gmail.com>
2025-11-02 13:57:30 -0400

    create app

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 import io
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch
-from reportlab.pdfgen import canvas
-from reportlab.lib.utils import ImageReader
-from reportlab.lib import colors
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
+import os
 
 # Configuración de la página
 st.set_page_config(
